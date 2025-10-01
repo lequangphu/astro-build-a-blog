083bc099e9e3e7d57b89b7dfacd4650a54cd3710
Phu <33654804+lequangphu@users.noreply.github.com>
2025-10-01 14:33:55 +0700

    add interaction

---
+++ src/scripts/menu.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('.menu');
+
+if (menu) {
+  menu.addEventListener('click', () => {
+    const isExpanded = menu.getAttribute('aria-expanded') === 'true';
+    menu.setAttribute('aria-expanded', (!isExpanded).toString());
+  });
+}
