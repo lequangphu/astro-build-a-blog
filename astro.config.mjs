66a14098047d6fd56232f45961065e000fbb6a0c
Phu <33654804+lequangphu@users.noreply.github.com>
2025-10-03 11:05:31 +0700

    add greeting and theme icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lequangphu.github.io',
   base: '/astro-build-a-blog',
+  integrations: [preact()],
 });
